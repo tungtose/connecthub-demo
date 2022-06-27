a867d0980652203125b5187ad0cd9fcf3082ec81
tungto <tungtose@gmail.com>
2022-06-27 14:56:51 +0700

    fix fetch conversation

--- src/hooks/useGetConversationSubcription.ts
+++ src/hooks/useGetConversationSubcription.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useQueryClient } from "react-query";
 import { GetConversationSubcriptionDocument } from 'src/generated/graphqlSdk';
+import { getToken } from "src/utils/jwt";
 
 const url = "ws://localhost:8080/v1/graphql";
 
@@ -8,8 +9,11 @@ export const useGetConversationSubcription = (id: string) => {
   const queryClient = useQueryClient();
   const [isSubscribing, setIsSubscribing] = useState(false);
   const [isSubscribingSuccess, setIsSubscribingSuccess] = useState(false);
+  const token = getToken();
 
   useEffect(() => {
+    if (!token) return;
+
     const ws = new WebSocket(url, "graphql-ws");
     setIsSubscribing(true);
 
@@ -19,7 +23,7 @@ export const useGetConversationSubcription = (id: string) => {
         payload: {
           // TODO: fix me!!!
           headers: {
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsidXNlciJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtdXNlci1pZCI6ImU5OWYwOWE3LWRkODgtNDlkNS1iMWM4LTFkYWY4MGMyZDdiMTgifSwiaWF0IjoxNjU1OTY3MzkwfQ.uD-_ZrU8BOToEIXF0bkncBdIr0aNp6hfTy9iTyLnuJA"
+            Authorization: `Bearer ${token}`
           }
         }
       }));
@@ -54,6 +58,6 @@ export const useGetConversationSubcription = (id: string) => {
       ws.send(JSON.stringify({ id: "1", type: "stop" }));
       ws.close();
     };
-  }, [queryClient]);
+  }, [queryClient, token]);
   return { isSubscribing, isSubscribingSuccess };
 };
